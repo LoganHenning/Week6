0016b75ffa79a064b72ec495b40d128e5a89ca6b
LoganHenning <lrhenning13@gmail.com>
2020-10-31 18:39:06 -0500

    Initial commit

---
+++ Week6project/src/Player.java
@@ -0,0 +1,46 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class Player {
+
+	
+
+		private List<Card>hand = new ArrayList<Card>();
+		private int score;
+		private String name;
+		
+		public Player() {
+			score = 0;
+		}
+	
+		public Player(String newName) {
+			name = newName;
+			score = 0;
+			
+		}
+		public void describe() {
+			System.out.printf("Player %s has the following cards: \n", name);
+			for (Card card:hand) {
+				card.describe();
+			}
+		}
+		public Card flip() {
+			return hand.remove(0);
+			
+		}
+		public void draw(Deck deck) {
+			hand.add(deck.draw());
+			
+		}
+		public void incrementScore() {
+			score++;
+		}
+		public int getScore() {
+			return score;
+			
+		}
+		
+	}
+
+
